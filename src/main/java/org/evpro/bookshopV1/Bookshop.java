2d801ca55f146d61ffca4bf9cbbd7a335ab1642c
saraobia <sara.obia99@gmail.com>
2024-08-27 13:54:36 +0200

    UPDATED: BookshopTest ADDED: Book exception to improve handle of methods

--- src/main/java/org/evpro/bookshopV1/Bookshop.java
+++ src/main/java/org/evpro/bookshopV1/Bookshop.java
@@ -15,42 +15,31 @@ import java.util.Set;
 public class Bookshop {
     private Set<Book> books = new HashSet<>();
 
-    //Methods
-    //Add book to the bookshop
     public boolean addBook(Book book) {
         return books.add(book);
     }
-
-    //GiveBook
     public boolean giveBook(String ISBN) {
         if (!isAvailable(ISBN)) {
-            return false;
+            throw new BookException("Book is not available");
         }
         searchBookByISBN(ISBN).setAvailable(false);
         return true;
     }
-
-    //ReturnBook
     public boolean returnBook(String ISBN) {
         if (isAvailable(ISBN)) {
-            return false;
+            throw new BookException("Book is already available");
         }
         searchBookByISBN(ISBN).setAvailable(true);
         return true;
     }
-
-
-    //Found book by ISBN
     public Book searchBookByISBN(String ISBN) {
-        for (Book book: books) {
+        for (Book book : books) {
             if (book.getISBN().equals(ISBN)) {
                 return book;
             }
         }
-        return null;
+        throw new BookException("Book with ISBN " + ISBN + " not found");
     }
-
-    //verify if the book is available
     private boolean isAvailable (String ISBN) {
         return searchBookByISBN(ISBN).isAvailable();
     }
--- src/test/java/BookshopTest.java
+++ src/test/java/BookshopTest.java
@@ -1,12 +1,16 @@
 import org.evpro.bookshopV1.Book;
+import org.evpro.bookshopV1.BookException;
 import org.evpro.bookshopV1.Bookshop;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-class BookshopTest {
+import static org.junit.Assert.*;
+
+public class BookshopTest {
 
     private Bookshop bookshop;
     private Book book1, book2, book3, book4, book5;
@@ -24,4 +28,88 @@ class BookshopTest {
     bookshop = new Bookshop(books);
     }
 
+    @Test
+    public void addBookIsGood() {
+        Book book6 = new Book("The Da Vinci Code", "Dan Brown","9780307474278",true);
+        int initialSize = bookshop.getBooks().size();
+
+        assertTrue(bookshop.addBook(book6));
+        assertEquals(initialSize + 1, bookshop.getBooks().size());
+        assertTrue(bookshop.getBooks().contains(book6));
+    }
+
+    @Test
+    public void giveBookIsGood() {
+        Book book7 = new Book("Brave New World", "Aldous Huxley","9780060850524",true);
+        bookshop.addBook(book7);
+
+        assertTrue(bookshop.giveBook(book7.getISBN()));
+    }
+
+    @Test()
+    public void giveBookIsBadNotExists() {
+        Book book8 = new Book("Book not added", "author", "9780060850533", true);
+        String invalidISBN = book8.getISBN();
+
+        BookException exception = assertThrows(BookException.class, () -> {
+            bookshop.giveBook(invalidISBN);
+        });
+
+        String expectedMessage = "Book with ISBN " + invalidISBN + " not found";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test()
+    public void giveBookIsBadNotAvailable() {
+        Book book9 = new Book("To Kill a Mockingbird", "Harper Lee", "9780446310789", false);
+        bookshop.addBook(book9);
+
+        BookException exception = assertThrows(BookException.class, () -> {
+            bookshop.giveBook(book9.getISBN());
+        });
+
+        String expectedMessage = "Book is not available";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void returnBookIsGood() {
+        Book book10 = new Book("Pride and Prejudice", "Jane Austen","9780141439518",false);
+        bookshop.addBook(book10);
+        assertTrue(bookshop.returnBook(book10.getISBN()));
+    }
+
+    @Test()
+    public void returnBookIsBadNotExists() {
+        Book book8 = new Book("Book not added", "author", "9780060850533", true);
+        String invalidISBN = book8.getISBN();
+
+        BookException exception = assertThrows(BookException.class, () -> {
+            bookshop.returnBook(invalidISBN);
+        });
+
+        String expectedMessage = "Book with ISBN " + invalidISBN + " not found";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test()
+    public void giveBookIsBadIsAvailable() {
+        Book book11 = new Book("To Kill a Mockingbird", "Harper Lee", "9780446310789", true);
+        bookshop.addBook(book11);
+
+        BookException exception = assertThrows(BookException.class, () -> {
+            bookshop.returnBook(book11.getISBN());
+        });
+
+        String expectedMessage = "Book is already available";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }
