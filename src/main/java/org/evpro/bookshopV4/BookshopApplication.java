09c6437a2b05c25412454775edf1ed323a0aec9d
saraobia <sara.obia99@gmail.com>
2024-08-27 19:46:11 +0200

    UPDATED; log conf

--- src/main/java/org/evpro/bookshopV4/BookshopApplication.java
+++ src/main/java/org/evpro/bookshopV4/BookshopApplication.java
@@ -1,9 +1,16 @@
 package org.evpro.bookshopV4;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BookshopApplication {
 
-    public static void main(String[] args) {
+        private static final Logger logger = LoggerFactory.getLogger(BookshopApplication.class);
 
+        public static void main(String[] args) {
+            logger.info("Test log message");
+            logger.error("Test error message");
+        }
     }
-}
+
--- src/main/java/org/evpro/bookshopV4/servlet/UserServlet.java
+++ src/main/java/org/evpro/bookshopV4/servlet/UserServlet.java
@@ -1,5 +1,9 @@
 package org.evpro.bookshopV4.servlet;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet("/users")
 public class UserServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Implement your logic here
         response.getWriter().write("Hello from UserServlet");
+        log.info("starting application ...");
     }
 }
