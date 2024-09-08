005989df14116820c7e3cfb1ae56ec5fcae58ab3
saraobia <sara.obia99@gmail.com>
2024-09-08 18:17:13 +0200

    UPDATED: pom.xml

---
+++ src/main/java/org/evpro/bookshopV5/BookShopApp.java
@@ -0,0 +1,12 @@
+package org.evpro.bookshopV5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopApp.class, args);
+    }
+
+}
