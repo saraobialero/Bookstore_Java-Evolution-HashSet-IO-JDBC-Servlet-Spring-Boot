d1a1a35ac0fec39505125e16e6cca868ae3ee012
saraobia <sara.obia99@gmail.com>
2024-09-14 12:48:43 +0200

    FIXED: logging bug, configuration logback in resources

--- src/main/java/org/evpro/bookshopV5/repository/RoleRepository.java
+++ src/main/java/org/evpro/bookshopV5/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import org.evpro.bookshopV5.model.Role;
 import org.evpro.bookshopV5.model.enums.RoleCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,9 +14,9 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     @Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Role r WHERE r.role = :roleCode")
-    boolean existsByRoleCode(RoleCode roleCode);
+    boolean existsByRoleCode(@Param("roleCode") RoleCode roleCode);
 
     @Query("SELECT r FROM Role r WHERE r.role = :roleCode")
-    Optional<Role> findByRoleCode(RoleCode roleCode);
+    Optional<Role> findByRoleCode(@Param("roleCode") RoleCode roleCode);
 
 }
